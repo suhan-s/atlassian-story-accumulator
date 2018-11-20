060360ce036b9948da2a72cb918a1a45cdbaf868
suhan.s <suhan.s@directi.com>
2018-11-20 06:13:31 +0530

    Taking queue url from profile

--- src/main/java/com/atlassian/acc/beans/Constants.java
+++ src/main/java/com/atlassian/acc/beans/Constants.java
@@ -11,9 +11,7 @@ public class Constants {
     public static final String SQS_REGION = ApplicationProperties.getProperty("SQS_REGION");
     public static final String SQS_ACCESS_KEY = ApplicationProperties.getProperty("SQS_ACCESS_KEY");
     public static final String SQS_SECRET_KEY = ApplicationProperties.getProperty("SQS_SECRET_KEY");
-    public static final String SQS_QUEUE_PATH = ApplicationProperties.getProperty("SQS_QUEUE_PATH");
-    public static final String SQS_QUEUE_NAME = "cst-test-queue";
-    public static final String SQS_QUEUE_URL = SQS_ENDPOINT + SQS_QUEUE_PATH + SQS_QUEUE_NAME;
+    public static final String SQS_QUEUE_URL = ApplicationProperties.getProperty("SQS_QUEUE_PATH");
 
     public static final String JIRA_BASE_URL = ApplicationProperties.getProperty("JIRA_BASE_URL");
     public static final String JIRA_API_PATH = "/rest/api/2/search";
